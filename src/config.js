c3264e37cbc1eb5b0a77b2f07e6c4eef80eec58b
Moo Park <amp0303@gmail.com>
2019-10-23 23:52:44 -0700

    Setting up basic page

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-west-1",
+      BUCKET: "cs172projectonefiles"
+    },
+    apiGateway: {
+      REGION: "us-west-1",
+      URL: "https://wqwisrgy5c.execute-api.us-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_6cYsNUOmZ",
+      APP_CLIENT_ID: "3aagi7s8ecse5j68n0e4ubs3pg",
+      IDENTITY_POOL_ID: "us-west-2:2a1e7713-b913-4f46-bad4-b4ed2851b906"
+    }
+  };
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Dropbox Clone</h1>
+        <p>UPLOAD SOMETHING BIHHHH</p>
+      </div>
+    </div>
+  );
+}
